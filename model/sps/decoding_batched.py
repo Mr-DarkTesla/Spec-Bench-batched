3707ded2315316f0386f3f5655d70ca4f02c3fcc
Mr-DarkTesla <yakushev.ga@phystech.edu>
2024-12-02 18:33:06 +0000

    WIP my sps == generation, but on long answers there are problems

--- model/sps/decoding_batched.py
+++ model/sps/decoding_batched.py
@@ -1,31 +1,67 @@
 import torch
 
-def attention_mask_4d(attention_mask_2d):
-    attention_mask_2d = attention_mask_2d.to(dtype=torch.float16)
-    device = attention_mask_2d.device
-    batch_size, seq_len = attention_mask_2d.shape
-
-    mask = torch.bmm(attention_mask_2d[:, :, None], attention_mask_2d[:, None, :])
-    tri = torch.ones(seq_len, seq_len).triu(diagonal=1) * float("-inf")
-    tri = torch.nan_to_num(tri).to(device)
-    mask += tri[None, :, :].repeat(batch_size, 1, 1)
-    return mask[:, None, :, :]
-
-
-def temperature_scaled_softmax(logits, temperature=1.0, dim=0):
-    res = torch.softmax(logits / max(temperature, 1e-3), dim=dim)
+def temperature_scaled_softmax(logits, temperature=0.0, dim=0):
+    assert not logits.isnan().any()
+    # if temperature > 0:
+    temperature = max(temperature, 1e-3)
+    res = torch.softmax(logits / temperature, dim=dim)
+    # else:
+    #     res = torch.argmax(logits, dim=dim)
     return res
+
+def check_mask(ids, mask, token_id):
+    batch_size, seq_len = mask.shape
+    for b in range(batch_size):
+        for idx, (i, m) in enumerate(zip(ids[b], mask[b])):
+            assert (m and i != token_id) or (not m and i == token_id), f"Batch: {b}, Index: {idx}, TokenId: {i}, Mask: {m}"
+
+@torch.no_grad()
+def sps_generate(
+    input_ids, 
+    attention_mask,
+    model, 
+    tokenizer, 
+    max_new_tokens,
+    tokens_per_forward=4,
+    drafter=None, 
+    do_sample=False, 
+    temperature=0.0
+):  
+    cum_accept = torch.zeros(len(input_ids)).to(input_ids.device)
+    step = 0
+    while True:
+        step += 1
+        input_ids, accept_length_tree, attention_mask, finished = sps_forward(
+            input_ids, attention_mask,
+            model,
+            tokenizer,
+            tokens_per_forward,
+            drafter=drafter,
+            do_sample=do_sample,
+            temperature=temperature,
+        )
+        cum_accept += accept_length_tree
+        if finished.all() or (cum_accept + step >= max_new_tokens).any():
+            return input_ids, step, cum_accept / step
 
 @torch.no_grad()
 def sps_forward(
-    input_ids, drafter_attention_mask,
-    # inputs,
-    verifier, tokenizer, max_new_tokens, drafter=None, do_sample=False, temperature=0.0):
-    # input_ids, drafter_attention_mask = inputs.input_ids, inputs.attention_mask
+    input_ids, 
+    drafter_attention_mask,
+    verifier, 
+    tokenizer, 
+    max_new_tokens,
+    drafter=None, 
+    do_sample=False, 
+    temperature=0.0
+):  
     drafter_position_ids = torch.cumsum(drafter_attention_mask, dim=-1)
     batch_size, input_len = input_ids.shape
     pad_token_id = tokenizer.pad_token_id
+    eos_token_id = tokenizer.eos_token_id
     device = input_ids.device
+
+    check_mask(input_ids, drafter_attention_mask, pad_token_id)
 
     # sps_sampling -> output_ids, idx, accept_length_list
     drafter_outputs = drafter.generate(
@@ -37,20 +73,27 @@ def sps_forward(
         temperature=temperature,
         return_dict_in_generate=True,
         output_scores=True)
-    drafter_input_ids, drafter_logits = drafter_outputs.sequences, torch.stack(drafter_outputs.scores, dim=1)
+    drafter_input_ids, drafter_logits = drafter_outputs.sequences, torch.stack(drafter_outputs.scores, dim=1).to(dtype=drafter.dtype)
     drafter_length = drafter_logits.shape[1]
 
     verifier_attention_mask = torch.cat((drafter_attention_mask,
                                 torch.ones((batch_size, max_new_tokens)).to(device)), 1)
     verifier_position_ids = torch.cumsum(verifier_attention_mask, dim=-1)
+
     verifier_logits = verifier( drafter_input_ids,
                                 attention_mask=verifier_attention_mask,
                                 position_ids=verifier_position_ids,
                                 do_sample=do_sample,
                                 temperature=temperature,
-                                ).logits                                    
-
-    _, n_matches, free_token = _speculative_sampling(
+                                ).logits
+    # print(verifier_attention_mask[:, -2*max_new_tokens:])
+    # print(drafter_input_ids[:, -2*max_new_tokens:])
+    # print(verifier_logits[:, -1-max_new_tokens:, :])
+    # print(torch.softmax(verifier_logits[:, -2, :], dim=-1))
+    # print(torch.argmax(verifier_logits[:, -2, :], dim=-1))
+    assert not verifier_logits.isnan().any()
+    # breakpoint()
+    n_matches, free_token = _speculative_sampling(
                 drafter_input_ids,
                 drafter_logits,
                 drafter_length,
@@ -59,22 +102,32 @@ def sps_forward(
                 do_sample=do_sample,
                 temperature=temperature,
             )
+    
+    # print(free_token)
 
-    # add padding
+    def pad_by_n(ids, n, max_tok):
+        max_n = max(n)
+        assert max_n < max_tok
+        mask = torch.arange(max_n)[None, :].to(device) < n[:, None]
+        padded = (ids[:, -max_tok:max_n-max_tok] - pad_token_id) * mask + pad_token_id
+        return padded, mask
+    
+    # add paddings
     max_n = max(n_matches)
     if max_n > 0:
-        mask = torch.arange(max_n)[None, :].to(device) < n_matches[:, None]
-        valid_tokens = (drafter_input_ids[:, -max_new_tokens:max_n-max_new_tokens] - pad_token_id) * mask + pad_token_id
-        output_ids = torch.cat((input_ids, valid_tokens, free_token), dim=-1)
+        valid_tokens, mask = pad_by_n(drafter_input_ids, n_matches, max_new_tokens)
+        new_tokens = torch.cat((valid_tokens, free_token), dim=-1)
+
+        output_ids = torch.cat((input_ids, new_tokens), dim=-1)
         attention_mask = torch.cat([drafter_attention_mask, mask], dim=1)
     else:
-        valid_tokens = torch.zeros(batch_size, 0).to(device)
         output_ids = torch.cat((input_ids, free_token), dim=-1)
         attention_mask = drafter_attention_mask
 
-    attention_mask = torch.cat((attention_mask, torch.ones((batch_size, 1)).to(device)), dim=1)
-    assert output_ids.shape == attention_mask.shape
-    return output_ids, valid_tokens, None, n_matches, attention_mask
+    finished = ((output_ids == eos_token_id).sum(dim=-1) > 0)
+    attention_mask = torch.cat((attention_mask, torch.ones((batch_size, 1)).to(device, dtype=int)), dim=1)
+    check_mask(output_ids, attention_mask, pad_token_id)
+    return output_ids, n_matches, attention_mask, finished
 
 
 @torch.no_grad()
@@ -92,28 +145,26 @@ def _speculative_sampling(
     
     new_candidate_input_ids = candidate_input_ids[:, -candidate_length:]
 
-    assert not candidate_logits.isnan().any()
     q = temperature_scaled_softmax(candidate_logits, temperature, dim=-1)
-    q_i = torch.gather(q, 2, new_candidate_input_ids[:, :, None]).squeeze(-1) #q[:, torch.arange(candidate_length), new_candidate_input_ids]
+    q_i = torch.gather(q, 2, new_candidate_input_ids[:, :, None]).squeeze(-1)
 
-    assert not new_logits.isnan().any()
     p = temperature_scaled_softmax(new_logits, temperature, dim=-1)
-    p_i = torch.gather(p, 2, new_candidate_input_ids[:, :, None]).squeeze(-1) #p[:, torch.arange(candidate_length), new_candidate_input_ids]
+    p_i = torch.gather(p, 2, new_candidate_input_ids[:, :, None]).squeeze(-1)
 
     probability_ratio = torch.clamp(p_i / q_i, min=0, max=1)
-    # print(p_i, q_i, probability_ratio, sep="\n\n")
 
     r_i = torch.rand_like(probability_ratio)
     is_accepted = r_i <= probability_ratio
-    n_matches = torch.minimum(((~is_accepted).cumsum(dim=-1) < 1).sum(dim=-1), torch.ones((batch_size), dtype=int).to(device) * max_new_tokens)
+
+    n_matches = ((~is_accepted).cumsum(dim=-1) < 1).sum(dim=-1)
+    max_new_tok_tensor = torch.ones((batch_size), dtype=int).to(device) * max_new_tokens
+    n_matches = torch.minimum(n_matches, max_new_tok_tensor)
     
     p_n_plus_1 = torch.take_along_dim(p, n_matches.resize(batch_size, 1, 1), 1)
     q_n_plus_1 = torch.take_along_dim(q, n_matches.resize(batch_size, 1, 1), 1)
     p_prime = torch.clamp((p_n_plus_1 - q_n_plus_1 * (n_matches < max_new_tokens).reshape(batch_size, 1, 1)), min=0).squeeze(1)
-
-    # print(p_prime)
-    if not do_sample:
+    if do_sample:
         t = torch.multinomial(p_prime, num_samples=1)
     else:
         t = torch.argmax(p_prime, dim=-1)[:, None]
-    return None, n_matches, t
+    return n_matches, t
--- run_batched.py
+++ run_batched.py
@@ -2,8 +2,8 @@ import torch
 import numpy as np
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
-from model.sps.decoding_batched import sps_forward
-from evaluation.inference_sps import sps_forward as sps_forward_test
+from model.sps.decoding_batched import sps_generate
+# from evaluation.inference_sps import sps_forward as sps_forward_test
 # from model.sps.decoding_batched import _speculative_sampling
 
 # from model.sps.decoding import assisted_decoding
@@ -13,26 +13,20 @@ from transformers.tokenization_utils_base import BatchEncoding
 def output_token_by_token(output_ids, tokenizer):
     print("OUTPUT:")
     for sample in output_ids:
-        # replace_pad = lambda s : s if s != "</s>" else ""
         print(tokenizer.decode(sample.cpu()).replace(pad_token, "").split("</s>")[0])
         print("------------------------------")
 
-
-# "meta-llama/Llama-2-7b-chat-hf" "TheBloke/Llama-2-7B-Chat-GPTQ" "meta-llama/Llama-3.1-8B-Instruct"
 model_path = "meta-llama/Llama-2-7b-chat-hf"
-# model_path = "meta-llama/Llama-3.1-8B-Instruct"
-# "TinyLlama/TinyLlama-1.1B-Chat-v1.0"  "JackFram/llama-68m" "meta-llama/Llama-3.2-1B-Instruct" 
 drafter_path = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-# drafter_path = "meta-llama/Llama-3.2-1B-Instruct" 
 
-device = "cuda:4"
+device = "cuda:0"
 dtype = "float16"
 temperature = 0
-max_new_tokens = 4
+max_new_tokens = 30
 
 model = AutoModelForCausalLM.from_pretrained(
     model_path,
-    quantization_config=BitsAndBytesConfig(load_in_8bit=True),
+    quantization_config=BitsAndBytesConfig(load_in_4bit=True),
     torch_dtype=str_to_torch_dtype(dtype),
     low_cpu_mem_usage=True,
     device_map = device,
@@ -40,15 +34,10 @@ model = AutoModelForCausalLM.from_pretrained(
 
 drafter = AutoModelForCausalLM.from_pretrained(
     drafter_path,
-    # quantization_config=BitsAndBytesConfig(load_in_8bit=True),
     torch_dtype=str_to_torch_dtype(dtype),
     low_cpu_mem_usage=True,
     device_map = device,
 )
-
-from transformers import GenerationMixin
-from model.sps.decoding import assisted_decoding
-GenerationMixin.assisted_decoding = assisted_decoding
 
 model.eval()
 drafter.eval()
@@ -58,23 +47,14 @@ if temperature > 0:
 else:
     do_sample = False
 tokenizer = AutoTokenizer.from_pretrained(model_path)
+pad_token = "[PAD]"
 if tokenizer.pad_token is None:
-    pad_token = "<s>"
     tokenizer.add_special_tokens({'pad_token': pad_token})
     model.resize_token_embeddings(len(tokenizer))
-pad_token_id = tokenizer.pad_token_id
+    drafter.resize_token_embeddings(len(tokenizer))
 
-# prompts = [
-#             # "What is the capital city of California state?",
-#             # "What do you think about global warming?",
-#             # "WTF is going on?",
-#             "Who plays young Damon in The Vampire Diaries?",
-#             "Tell me a story.",
-#             "Continue sequence 1 2 3 4 5 6:",
-#             # "Count from 1 to 10:",
-#             # "9 8 7 6 5",
-#             # "a b c d"
-#           ]
+pad_token = tokenizer.pad_token
+pad_token_id = tokenizer.pad_token_id
 
 prompts = [
     "Translate German to English: „ Das verändert die Zukunft meiner Familie “ , meinte der Mann .",
@@ -82,11 +62,7 @@ prompts = [
     "Translate German to English: Als Zeuge war der damals ermittelnde Kommissar geladen .",
     "Translate German to English: Je dunkler das Fleisch , desto höher der ph-Wert .",
 ]
-# SPACE IN THE END IS IMPORTANT FOR SPECBENCH SPS
 prompt_template=[f"[INST] {prompt} [/INST]" for prompt in prompts]
-
-iterations = 15
-
 
 # My batched sps
 print("\nMy SPS:")
@@ -96,46 +72,33 @@ inputs = tokenizer( prompt_template,
                         padding="longest", 
                         return_attention_mask=True, 
                         return_tensors="pt",
-                        padding_side="right").to(device)
+                        padding_side="left"
+                        ).to(device)
 input_ids, attention_mask = inputs.input_ids, inputs.attention_mask
-
-for _ in range(iterations):
-    input_ids, step, _, accept_length_tree, attention_mask = sps_forward(
-                            input_ids, attention_mask,
-                            model,
-                            tokenizer,
-                            max_new_tokens,
-                            drafter=drafter,
+input_ids, step, accept_length = sps_generate(
+                        input_ids, attention_mask,
+                        model,
+                        tokenizer,
+                        max_new_tokens,
+                        drafter=drafter,
                             do_sample=do_sample,
                             temperature=temperature,
-                        )
-    # print(accept_length_tree)
+                    )
+print("AVG ACCEPT:", accept_length)
 output_token_by_token(input_ids, tokenizer)
 
-
-# # Original sps frmo spec bench
-# print("\nOriginal SPS frmo spec bench:")
-# for prompt in prompt_template:
-#     inputs = tokenizer([prompt], return_tensors="pt").to(device).input_ids
-#     for i in range(iterations):
-#         inputs = sps_forward_test(
-#                     inputs,
-#                     model,
-#                     tokenizer,
-#                     max_new_tokens,
-#                     drafter=drafter,
-#                     do_sample=do_sample,
-#                     temperature=temperature)
-#     output_token_by_token(inputs, tokenizer)
-
-# Just model 
+# Just model generation
 print("\nModel Generation:")
 for prompt in prompt_template:
-    inputs = tokenizer([prompt], return_tensors="pt").to(device).input_ids
-    for i in range(2 * iterations):
-        inputs = model.generate(
-        inputs,
-        max_new_tokens=max_new_tokens,
-        do_sample=do_sample,
-        temperature=temperature)
-    output_token_by_token(inputs, tokenizer)
+    input_ids = tokenizer([prompt], return_tensors="pt").to(device).input_ids
+    inputs = model.generate(
+    input_ids,
+    max_new_tokens=max_new_tokens,
+    do_sample=do_sample,
+    temperature=temperature,
+    return_dict_in_generate=True,
+    output_scores=True)
+    # input_ids, logits = inputs.sequences, torch.stack(inputs.scores, dim=1)
+    # print(torch.softmax(logits[0][0], dim=-1))
+    # print(torch.argmax(logits[0][0], dim=-1))
+    output_token_by_token(inputs.sequences, tokenizer)
