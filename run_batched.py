5291fb317eb7ffd12d52fed66a55102377b758f1
Mr-DarkTesla <yakushev.ga@phystech.edu>
2024-12-02 19:40:26 +0000

    WIP added checker with generation

--- run_batched.py
+++ run_batched.py
@@ -10,11 +10,44 @@ from model.sps.decoding_batched import sps_generate
 from fastchat.utils import str_to_torch_dtype
 from transformers.tokenization_utils_base import BatchEncoding
 
-def output_token_by_token(output_ids, tokenizer):
-    print("OUTPUT:")
-    for sample in output_ids:
-        print(tokenizer.decode(sample.cpu()).replace(pad_token, "").split("</s>")[0])
-        print("------------------------------")
+class ResponseChecker:
+    def __init__(self, tokenizer, prompts):
+        self.prompts = prompts
+        self.target = []
+        self.pred = []
+        self.tokenizer = tokenizer
+        self.pad_token = tokenizer.pad_token
+    
+    def ids_to_str(self, ids):
+        return self.tokenizer.decode(ids.cpu()).replace(self.pad_token, "").split("</s>")[0]
+    
+    def add_target(self, ids):
+        for sample in ids:
+            s = self.ids_to_str(sample)
+            self.target.append(s)
+    
+    def add_pred(self, ids):
+        for sample in ids:
+            s = self.ids_to_str(sample)
+            self.pred.append(s)
+
+    def run_check(self):
+        assert len(self.target) == len(self.prompts), f"{len(self.target)} {len(self.prompts)}"
+        assert len(self.pred) == len(self.prompts), f"{len(self.pred)} {len(self.prompts)}"
+        for prompt, pred, target in zip(self.prompts, self.pred, self.target):
+            length = min(len(pred), len(target))
+            if pred[:length] != target[:length]:
+                print(  f"Error on prompt: {prompt}\n" \
+                        f"Target    : {target}\n" \
+                        f"Prediction: {pred}\n")
+        print("Check finished")
+    
+    @staticmethod
+    def output_token_by_token(output_ids, tokenizer):
+        print("OUTPUT:")
+        for sample in output_ids:
+            print(tokenizer.decode(sample.cpu()).replace(pad_token, "").split("</s>")[0])
+            print("------------------------------")
 
 model_path = "meta-llama/Llama-2-7b-chat-hf"
 drafter_path = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
@@ -61,11 +94,16 @@ prompts = [
     "Translate German to English: \" Das habe ich verboten \" , erklärte er .",
     "Translate German to English: Als Zeuge war der damals ermittelnde Kommissar geladen .",
     "Translate German to English: Je dunkler das Fleisch , desto höher der ph-Wert .",
+    "Random test 1",
+    "Something to generate",
+    "Blabla bla",
+    "ewfj2nfo f32ipojd;qnf 32oirj0[923jfqe  39r0uq23'j]",
 ]
 prompt_template=[f"[INST] {prompt} [/INST]" for prompt in prompts]
 
+checker = ResponseChecker(tokenizer, prompt_template)
+
 # My batched sps
-print("\nMy SPS:")
 inputs = tokenizer( prompt_template, 
                         add_special_tokens=True,
                         # truncation=True, 
@@ -85,10 +123,9 @@ input_ids, step, accept_length = sps_generate(
                             temperature=temperature,
                     )
 print("AVG ACCEPT:", accept_length)
-output_token_by_token(input_ids, tokenizer)
+checker.add_pred(input_ids)
 
 # Just model generation
-print("\nModel Generation:")
 for prompt in prompt_template:
     input_ids = tokenizer([prompt], return_tensors="pt").to(device).input_ids
     inputs = model.generate(
@@ -98,7 +135,5 @@ for prompt in prompt_template:
     temperature=temperature,
     return_dict_in_generate=True,
     output_scores=True)
-    # input_ids, logits = inputs.sequences, torch.stack(inputs.scores, dim=1)
-    # print(torch.softmax(logits[0][0], dim=-1))
-    # print(torch.argmax(logits[0][0], dim=-1))
-    output_token_by_token(inputs.sequences, tokenizer)
+    checker.add_target(inputs.sequences)
+checker.run_check()
